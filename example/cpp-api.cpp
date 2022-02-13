472b8d78c4f4ce16fb0158d79c4c895f7f4c1c33
Edward J. Schwartz <eschwartz@cert.org>
2022-02-13 10:27:24 -0500

    fillwords

--- example/cpp-api.cpp
+++ example/cpp-api.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #include "prettyprint.h"
 
@@ -18,14 +19,51 @@ void print_basic() {
              << std::endl << std::endl;
 }
 
-void print_grouped() {
-    auto doc = pp::group( pp::nil() +
-            "one" + pp::line() +
-            "two" + pp::line() +
-            "three" + pp::line() +
-            "four");
+std::shared_ptr<pp::doc> fillwords (const std::vector<std::string> &v) {
+  auto doc = pp::nil ();
+  bool first = true;
 
-    auto settings = std::cout << pp::set_width(40);
+  for (auto it = v.begin (); it != v.end (); it++) {
+    if (first) {
+      doc = (doc + *it);
+      first = false;
+    } else
+      doc = (doc + pp::group (pp::line ()) + *it);
+  }
+
+  return doc;
+}
+
+void print_grouped() {
+    // auto doc = pp::group( pp::nil() +
+    //         "one" + pp::line() +
+    //         "two" + pp::line() +
+    //         "three" + pp::line() +
+    //         "four");
+
+
+  std::vector<std::string> v = {"Rerum",
+    "eveniet",
+    "dolorem",
+    "ducimus.",
+    "Autem",
+    "sit",
+    "explicabo",
+    "dolorem.",
+    "Dolorem",
+    "veniam",
+    "velit",
+    "maiores",
+    "rem",
+    "qui",
+    "et",
+    "laudantium",
+    "deleniti."};
+
+  auto doc = fillwords (v);
+  //auto doc == pp::nil () 
+
+    auto settings = std::cout << pp::set_width(30);
     settings << pp::set_max_indent(20) << doc
              << std::endl << std::endl;
 
